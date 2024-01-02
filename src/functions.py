18f9f9d91aa29d36a5800a59886ccc0a6776b7dd
RuleBrittonica <mbritton460@gmail.com>
2024-01-02 23:03:27 +1100

    going to bed

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 from time import sleep
 
 def start_chrome():
@@ -20,6 +21,16 @@ def scraper_main():
 
     driver = get_to_library(driver) # get to the library page
 
+    driver = new_contributor(driver)
+
+    driver = connection_settings(driver)
+
+    driver = processing_steps(driver)
+
+    driver = test_harvest(driver)
+
+    driver = logs(driver)
+
 
 
     driver.close()
@@ -37,3 +48,35 @@ def get_to_library(driver):
 
     return driver
 
+
+def new_contributor(driver):
+    from_first_path = ""
+    from_first_box = driver.find_element('css selector', from_first_path)
+    select_from_first = Select(from_first_box)
+    from_first_box.click()
+    sleep(0.5)
+
+    desired_option = ""
+    select_from_first.select_by_visible_text(desired_option)
+
+    # To select by index (0-based index):
+    # select_anbd.select_by_index(index)
+
+    # To select by value attribute:
+    # select_anbd.select_by_value(value)
+    return driver
+
+def connection_settings(driver):
+    pass
+
+def data_storage_settings(driver):
+    pass
+
+def processing_steps(driver):
+    pass
+
+def test_harvest(driver):
+    pass
+
+def logs(driver):
+    pass

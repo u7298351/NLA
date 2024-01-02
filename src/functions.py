d073ed2cd6f758febdfc5a1f1ea92cb0358a1e59
RuleBrittonica <mbritton460@gmail.com>
2024-01-02 22:48:33 +1100

    lachie get here

--- src/functions.py
+++ src/functions.py
@@ -2,3 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 
+def start_chrome():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-extensions")
+    # chrome_options.add_argument("--headless") # Enable this option when code
+    # is working fine to run in headless mode (without opening browser)
+
+    driver = webdriver.Chrome(options=chrome_options)
+    url = ""
+    driver.get(url)
+    return driver
+
+def scraper_main():
+    driver = start_chrome()
+    #  define actions using subfunctions and selenium here
+
+
+
+    driver.close()
+
+def get_to_library():
+    
+
